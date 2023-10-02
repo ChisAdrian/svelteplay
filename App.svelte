601c9f7cfb41bdbb9f7957795132f3e6a74ef293
ChisAdrian <44084674+ChisAdrian@users.noreply.github.com>
2023-10-02 14:01:20 +0300

    Update App.svelte

--- App.svelte
+++ App.svelte
@@ -1,70 +1,59 @@
-
 <script>
 	import DataTbl from './DataTbl.svelte';
+  
 	export const router = null;
+  
 	let fontSize = 16;
-	
-	let tData = [['*']]
-
-  const getPhpResponce = async () => 
-  {
-    const response = await fetch('mimic_php_responce.php'
-		, {
+	let tbl_data_cols = [['*']];
+	let tbl_data_rows = [['*']];
+  
+	const getPhpResponse = async () => {
+	  const response = await fetch('mimic_php_responce.php', {
 		method: 'POST',
 		headers: {
-			"Content-Type": "application/json"
-			},
-		body: JSON.stringify({ username: "UN" , pass :"PCXPD" })
-		}
-		
-	);
+		  'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ username: 'UN', pass: 'PCXPD' })
+	  });
   
-	const json = await response.json();
-
-	tData = json;
-  }
-
-</script>
-
-<main>
-	<button on:click={getPhpResponce}>GetPHP</button>
-	<button on:click={e => fontSize--}>-</button>
-	
-		<div style="font-size: {fontSize + 'px'}">
-			<div id="tbl_data_parent">
-				<DataTbl  tbl_data= {tData} />
-			</div>
-		</div>
-	
-
-</main>
-
-<style>
-
-	#tbl_data_parent
-	{
-		height: 84vh;
-		overflow: auto;
-		/*border-radius: 5%;
-		border-color: rgb(87, 79, 79);
-		border-style: solid;
-		*/
+	  const [columns, rows] = await response.json();
+  
+	  tbl_data_cols = columns;
+	  tbl_data_rows = rows;
+	};
+  </script>
+  
+  <main>
+	<button on:click={getPhpResponse}>GetPHP</button>
+	<button on:click={() => (fontSize -= 1)}>-</button>
+  
+	<div style="font-size: {fontSize}px">
+	  <div id="tbl_data_parent">
+		<DataTbl bind:tbl_data_cols={tbl_data_cols} bind:tbl_data_rows={tbl_data_rows} />
+	  </div>
+	</div>
+  </main>
+  
+  <style>
+	#tbl_data_parent {
+	  height: 84vh;
+	  overflow: auto;
 	}
-
+  
 	main {
-		background-color: white;
-		height: 100%;
+	  background-color: white;
+	  height: 100%;
 	}
-
-	button{
-		border-radius: 10%;
-		background-color: lightslategray;
-		color: whitesmoke;
-		border-width: 3px;
+  
+	button {
+	  border-radius: 10%;
+	  background-color: lightslategray;
+	  color: whitesmoke;
+	  border-width: 3px;
 	}
-
-
-	button:hover{
-		background-color: rgb(5, 25, 44);
+  
+	button:hover {
+	  background-color: rgb(5, 25, 44);
 	}
-</style>
+  </style>
+  
