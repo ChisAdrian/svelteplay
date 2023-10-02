4a421c1c134832b97d90cb26ed3de12c62d038a2
ChisAdrian <44084674+ChisAdrian@users.noreply.github.com>
2023-10-02 13:44:58 +0300

    Update DataTbl.svelte

--- DataTbl.svelte
+++ DataTbl.svelte
@@ -1,33 +1,66 @@
 <script>
-    export let tbl_data;
-</script>
-
-<div>
+    export let tbl_data_cols;
+    export let tbl_data_rows;
+  
+    let sortBy = { col: 0, ascending: true };
+  
+    function isNumber(value) {
+      return !isNaN(parseFloat(value)) && isFinite(value);
+    }
+  
+    function sort(column) {
+      sortBy.ascending = sortBy.col === column ? !sortBy.ascending : true;
+      sortBy.col = column;
+  
+      tbl_data_rows = tbl_data_rows.sort((a, b) => {
+        const sortModifier = sortBy.ascending ? 1 : -1;
+        const aValue = a[column];
+        const bValue = b[column];
+  
+        return isNumber(aValue)
+          ? sortModifier * (aValue - bValue)
+          : aValue.localeCompare(bValue) * sortModifier;
+      });
+    }
+  </script>
+  
+  <div>
     <table>
-        <thead>
+      <thead>
         <tr>
-            {#each tbl_data[0] as th}
-                <th>{th}</th>
-            {/each}
+          {#each tbl_data_cols as th, i}
+            <th on:click={() => sort(i)} class:selected={sortBy.col === i} class:asc={sortBy.col === i && sortBy.ascending} class:desc={sortBy.col === i && !sortBy.ascending}>
+              {th}
+            </th>
+          {/each}
         </tr>
-    </thead>
-    <tbody>
-        {#each tbl_data as row, i}
-            <tr>
-                {#each row as c}
-                    {#if i > 0}
-                        <td>{c}</td> 
-                    {/if}
-                {/each}
-            </tr>
-            <!-- on:click={increment}-->
+      </thead>
+      <tbody>
+        {#each tbl_data_rows as row, i}
+          <tr key={i}>
+            {#each row as c}
+              <td>{c}</td>
+            {/each}
+          </tr>
         {/each}
-    </tbody>
+      </tbody>
     </table>
-</div>
-
-<style>
-    
+  </div>
+  
+  <style>
+    .selected {
+      font-weight: bold;
+    }
+  
+    .asc::after {
+      content: " \2191";
+    }
+  
+    .desc::after {
+      content: " \2193";
+    }
+  
+    /* Add other styles as needed */
 
     thead{
         position: sticky;
@@ -53,4 +86,5 @@
     td {
         text-align: center;
     }
-</style>
+  </style>
+  
