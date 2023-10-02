495f1262b4ea18fe726052264f2fbe196a371494
ChisAdrian <44084674+ChisAdrian@users.noreply.github.com>
2023-10-02 14:00:59 +0300

    Update mimic_php_responce.php

--- mimic_php_responce.php
+++ mimic_php_responce.php
@@ -1,23 +1,28 @@
 <?php
+// Initialize an array to hold the response
+$RESP = array();
 
-$somrRR = array();
+// Define column names
+$cols = array('NR', 'Col2', 'Col3', 'Col4');
 
-//$data = json_decode(trim(file_get_contents("php://input")) ,true);
+// Initialize an array to hold the rows
+$rows = array();
 
-//$string_version = implode(',', $data);
-
- //array_push($somrRR , array($string_version ));
-
-
-array_push($somrRR ,  array ('NR','Col2','Col3','Col4'));
-
+// Generate 102 rows of data
 for ($x = 1; $x <= 102; $x++) {
-    
-    array_push($somrRR ,   
-    array ( strval($x) , 'someV' . strval($x)  , 'someV2' . strval($x*10) ,  '****'  ) );
-  }
+    // Each row contains an array with numeric and string values
+    array_push($rows, array(
+        strval($x),                     // Numeric value
+        'someV' . strval($x),           // 'someV' followed by the numeric value
+        'someV2' . strval($x * 10),     // 'someV2' followed by the numeric value times 10
+        '****'                          // Constant string '****'
+    ));
+}
 
+// Push the column names and rows into the response array
+array_push($RESP, $cols);
+array_push($RESP, $rows);
 
-
-echo json_encode($somrRR);
+// Encode the response array to JSON and echo it
+echo json_encode($RESP);
 ?>
